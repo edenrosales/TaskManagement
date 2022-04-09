47ae3670a4e91a75c233de62cc504166ddb283f2
Priaec <joshcohen8020@gmail.com>
2022-04-09 15:03:11 -0700

    Added two methods to To-Do List

--- app/src/main/java/com/example/taskmanagement/TodoList.java
+++ app/src/main/java/com/example/taskmanagement/TodoList.java
@@ -1,4 +1,14 @@
 package com.example.taskmanagement;
 
 public class TodoList {
+    //Joshua is here and will start to experiment a bit with the todo list class
+
+    public void getTaskList(){
+
+    }
+
+    public void displayTaskList(){
+
+    }
+
 }
