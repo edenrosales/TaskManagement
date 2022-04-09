d1cad928a17f434baa420c3f06fa1b3e9b984601
edenrosales <eden@rosales5.com>
2022-04-09 14:34:15 -0700

    Implementing Tag and Task classes
    Basic implementation for the Tag and the Task class. I did this so that the other classes can be worked on without needing the use of blackbox functions or any other workarounds. Left out some functions like the deleteTask() function, so that the people who are responsible for this component can implement.

--- app/src/main/java/com/example/taskmanagement/Tag.java
+++ app/src/main/java/com/example/taskmanagement/Tag.java
@@ -1,4 +1,24 @@
 package com.example.taskmanagement;
 
+import android.graphics.Color;
+
 public class Tag {
+    String name;
+    String color;
+    public void setName(String name){
+        name = name;
+    }
+    public String getName() {
+        if(!name.isEmpty())
+        return name;
+        else return null;
+    }
+    public void setColor(String color){
+        color = color;
+    }
+    public String getColor(){
+        if(!color.isEmpty())
+            return color;
+        else return null;
+    }
 }
--- app/src/main/java/com/example/taskmanagement/Task.java
+++ app/src/main/java/com/example/taskmanagement/Task.java
@@ -1,4 +1,72 @@
 package com.example.taskmanagement;
 
 public class Task {
+    String name;
+    Tag associated_tag;
+    int start_time;
+    int end_time;
+    int due_date;
+    Boolean notify_me;
+    Task(String name,Tag associated_tag,int start_time,int end_time,int due_date, Boolean notify_me){
+        name = name;
+        associated_tag = associated_tag;
+        end_time = end_time;
+        due_date = due_date;
+        notify_me = notify_me;
+        start_time = start_time;
+    }
+    public void createTask(){
+        //this method should invoke the setter method but it should be made so that the user only needs to put in certain parts of the function
+        //for instance, if the user only puts in a name, but no tag, the method should have a case for that, and if they put a start time and no end time there should
+        //also be a case for that in the code... i dont know how to do that so i will leave that to Hampig, or whoever is working on this class.
+        //-Eden
+    }
+    public void setName(String name){
+        name = name;
+    }
+    public String getName(){
+        if(!name.isEmpty()){
+            return name;
+        }
+        else return null;
+    }
+    public void setTag(Tag associated_tag){
+        associated_tag = associated_tag;
+    }
+    public Tag getTag(){
+        if(associated_tag!=null)
+            return associated_tag;
+        else return null;
+    }
+    public void setStart(int start_time){
+        start_time = start_time;
+    }
+    public int getStart(){
+        return start_time;
+    }
+    public void setEnd(int end_time){
+        end_time = end_time;
+    }
+    public int getEnd(){
+        return end_time;
+    }
+    public void setDue(int due_date){
+        due_date = due_date;
+    }
+    public int getDue(){
+        return due_date;
+    }
+    public void setNotify(Boolean notify_me){
+        notify_me = notify_me;
+    }
+    public Boolean getNotify(){
+        return notify_me;
+    }
+    //this function should go to the database and delete a task, if it is successful, then it should return true, otherwise false.
+    //we could associate an error message to see what the problem is if it does not delete
+    public Boolean deleteTask(String task_name){
+        return true;
+    }
+
+
 }
