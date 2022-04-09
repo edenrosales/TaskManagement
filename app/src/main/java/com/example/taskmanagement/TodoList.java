7c82944b4726f9bbd35269d9a03588b92a7f29b9
Priaec <joshcohen8020@gmail.com>
2022-04-09 15:27:18 -0700

    Added comments to set up for writing functions on the todo list and calendar files

--- app/src/main/java/com/example/taskmanagement/TodoList.java
+++ app/src/main/java/com/example/taskmanagement/TodoList.java
@@ -1,14 +1,15 @@
 package com.example.taskmanagement;
+import java.util.ArrayList;
 
 public class TodoList {
     //Joshua is here and will start to experiment a bit with the todo list class
 
-    public void getTaskList(){
-
+    public void getTaskList(ArrayList<Tag> tags){
+    //method will return a list of the necessary tasks that are invoked by a specific filter or a set of filters
     }
 
     public void displayTaskList(){
-
+    //method will show the tasks obtained from getTaskList() method in a to-do-list fashion
     }
 
 }
