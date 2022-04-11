329fde8353b6dcc8a01a324bf98b4c6a53def517
edenrosales <eden@rosales5.com>
2022-04-10 19:35:15 -0700

    Comments on Database Design

--- app/src/main/java/com/example/taskmanagement/Database.java
+++ app/src/main/java/com/example/taskmanagement/Database.java
@@ -2,5 +2,12 @@ package com.example.taskmanagement;
 
 
 //I do not know what this class is responsible for, I need more clarification and examples
+//Clarification from Eden:
+/* I dont know if we need a class to do this, however we need a way to store tasks that the user wants to keep in a way that is not just temporary.
+*   I have a feeling if we just used a large vector to store all the data that we need from sessoin to session on the app, it would be temporary and the phone would delete it
+*   So we need a more permenant solution for this. As it stands now, I think that it actually is ok that we dont have a database, as we can test all our methods with
+*   functions that only use temporary, made-up data, however this is still a very imporatnat problem that needs to be solved soon.
+*
+*   Also, thank you Pouria for researching this. */
 public class Database {
 }
