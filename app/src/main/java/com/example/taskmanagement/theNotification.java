93f1d81c8d1e8d12d86b8d90cd72d84b47917cd4
edenrosales <eden@rosales5.com>
2022-04-10 19:38:39 -0700

    More comments.

--- app/src/main/java/com/example/taskmanagement/theNotification.java
+++ app/src/main/java/com/example/taskmanagement/theNotification.java
@@ -2,5 +2,6 @@ package com.example.taskmanagement;
 
 
 //Responsible for sending pop up notifications to user's phone
+//depending on how android studio handles notifications, we might not need this class.
 public class theNotification {
 }
