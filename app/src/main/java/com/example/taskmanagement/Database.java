5fe94905d6ad789576d2eeacee8dc524eb5e474f
Pouria Sal <thesalekani@gmail.com>
2022-04-11 21:14:45 -0700

    Following up comments

--- app/src/main/java/com/example/taskmanagement/Database.java
+++ app/src/main/java/com/example/taskmanagement/Database.java
@@ -9,5 +9,22 @@ package com.example.taskmanagement;
 *   functions that only use temporary, made-up data, however this is still a very imporatnat problem that needs to be solved soon.
 *
 *   Also, thank you Pouria for researching this. */
+
+
+/*
+       Response from Pouria:
+No problem. I did some research and wrote a lot in detail in the Jira post but now that I understand what we need...
+we definitely need this class.
+    This class would be responsible for the following:
+        Adding Tasks
+        Deleting Tasks
+        Editing Tasks
+        When user exists out of app, tasks stays
+
+There are several ways to implement this, if you look on the Jira post, you can find them.
+I say we use the Room library, but I would have to do some tests on my own to see which is best.
+
+
+ */
 public class Database {
 }
--- app/src/main/java/com/example/taskmanagement/theNotification.java
+++ app/src/main/java/com/example/taskmanagement/theNotification.java
@@ -3,5 +3,10 @@ package com.example.taskmanagement;
 
 //Responsible for sending pop up notifications to user's phone
 //depending on how android studio handles notifications, we might not need this class.
+
+//Responding to this ^
+//Android Studio already has a built in method for handling Notifications but I think...
+//depending on how *we* handle notifications, we might need this
+//If we only show a notification when a task is near its date then we dont need it, imo
 public class theNotification {
 }
