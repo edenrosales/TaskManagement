71e5adf5c1a621c36f346c6ed3cfe34712a40218
edenrosales <eden@rosales5.com>
2022-04-24 17:24:47 -0700

    Adding Date/Time functionality in the classes
    in progress...

--- app/src/main/java/com/example/taskmanagement/Calendar.java
+++ app/src/main/java/com/example/taskmanagement/Calendar.java
@@ -4,13 +4,16 @@ public class Calendar {
 
     public void viewPresent(){
     //method will show the current calendar day in the calendar view
+
     }
 
     public void getTaskList(ArrayList<Tag> tags){
     //same method as getTaskList() in to-do List except this is optimized for calendar view, so sorted by starting and ending time
+
     }
 
     public void displayCalendar(){
     //method to invoke the calendar view. This function will show all the necessary data that needs to be displayed at a given moment
+
     }
 }
--- app/src/main/java/com/example/taskmanagement/TodoList.java
+++ app/src/main/java/com/example/taskmanagement/TodoList.java
@@ -1,10 +1,16 @@
 package com.example.taskmanagement;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Date;
+import java.time.Month;
+import java.time.LocalDate;
 
 public class TodoList {
     //Joshua is here and will start to experiment a bit with the todo list class
+    LinkedList<Task> todo = new LinkedList<>();
 
-    public void getTaskList(ArrayList<Tag> tags){
+    public void getTaskList(ArrayList<Tag> tags){//we will want to only get the tasks that should be loaded for this day/ interval
+
     //method will return a list of the necessary tasks that are invoked by a specific filter or a set of filters
     }
 
