6c56a89e9ac5bc02344bae5d7d6dee00a24275a8
edenrosales <eden@rosales5.com>
2022-04-26 12:52:29 -0700

    Commenting out the DB. It wasnt working right

--- app/src/main/java/com/example/taskmanagement/Database.java
+++ app/src/main/java/com/example/taskmanagement/Database.java
@@ -1,28 +1,28 @@
-package com.example.taskmanagement;
-
-
-import android.content.Context;
-
-import androidx.room.Room;
-import androidx.room.RoomDatabase;
-
-//Database class, holds all info
-
-@androidx.room.Database(entities = {Task.class}, version = 1) //DO NOT TOUCH
-public abstract class Database extends RoomDatabase {
-
-    public static Database INSTANCE;
-
-    public synchronized static Database getDatabase(Context context){
-        if(INSTANCE == null){ //Making the database when does not exist
-            INSTANCE = Room.databaseBuilder(context.getApplicationContext()
-                    ,Database.class, "DATABASE_1")
-                    .allowMainThreadQueries()
-                    .fallbackToDestructiveMigration()
-                    .build();
-        }
-        return INSTANCE;
-    }
-
-    public abstract DatabaseDAO databaseDAO();
-}
+//package com.example.taskmanagement;
+//
+//
+//import android.content.Context;
+//
+//import androidx.room.Room;
+//import androidx.room.RoomDatabase;
+//
+////Database class, holds all info
+//
+//@androidx.room.Database(entities = {Task.class}, version = 1) //DO NOT TOUCH
+//public abstract class Database extends RoomDatabase {
+//
+//    public static Database INSTANCE;
+//
+//    public synchronized static Database getDatabase(Context context){
+//        if(INSTANCE == null){ //Making the database when does not exist
+//            INSTANCE = Room.databaseBuilder(context.getApplicationContext()
+//                    ,Database.class, "DATABASE_1")
+//                    .allowMainThreadQueries()
+//                    .fallbackToDestructiveMigration()
+//                    .build();
+//        }
+//        return INSTANCE;
+//    }
+//
+//    public abstract DatabaseDAO databaseDAO();
+//}
