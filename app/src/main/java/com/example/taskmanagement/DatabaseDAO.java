8b7cd8cbec6e5b8bc0d0108df0a35069506d1441
Pouria Sal <thesalekani@gmail.com>
2022-05-04 16:44:22 -0700

    Removed unnecssary brackets in Database classes

--- app/src/main/java/com/example/taskmanagement/DatabaseDAO.java
+++ app/src/main/java/com/example/taskmanagement/DatabaseDAO.java
@@ -1,32 +1,32 @@
-//package com.example.taskmanagement;
-//
-//import java.util.List;
-//
-//import androidx.room.Dao;
-//import androidx.room.Delete;
-//import androidx.room.Insert;
-//import androidx.room.Query;
-//import androidx.room.Update;
-//
+package com.example.taskmanagement;
+
+import java.util.List;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
 ////Responsible for:
 //// Editing Task
 //// Adding Task
 //// Deleting Task
 //// Reading Task
-//
-//
-//@Dao
-//public interface DatabaseDAO {
-//    @Query("SELECT * FROM task") //Reads Tasks
-//    List<Task> getAll();
-//
-//    @Insert
-//    void insert(Task task);
-//
-//    @Delete
-//    void delete(Task task);
-//
-//    @Update
-//    void update(Task task); //Will fix this soon, some reason, editing does not save
-//
-//}
+
+
+@Dao
+public interface DatabaseDAO {
+    @Query("SELECT * FROM task") //Reads Tasks
+    List<Task> getAll();
+
+    @Insert
+    void insert(Task task);
+
+    @Delete
+    void delete(Task task);
+
+    @Update
+    void update(Task task); //Will fix this soon, some reason, editing does not save
+
+}
